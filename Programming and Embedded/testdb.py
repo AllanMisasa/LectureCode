d1d8de0d4b97a4a0afe6d0d1cab9c305f688cb69
AllanMisasa <Singulytics@gmail.com>
2022-04-29 17:08:10 +0200

    added sqlite and dust sensor code

--- Programming and Embedded/testdb.py
+++ Programming and Embedded/testdb.py
@@ -18,23 +18,48 @@ conn = sqlite3.connect(r'C:/db/tttest.db')
 c = conn.cursor()
 
 sql = '''CREATE TABLE MEASUREMENTS(
-   DEVICE_ID INTEGER PRIMARY KEY,
    TEMPERATURE INT NOT NULL,
    HUMIDITY INT NOT NULL,
     DUST FLOAT NOT NULL,
     CO2 INT NOT NULL
 )'''
 
-# c.execute(sql)
+c.execute(sql)
 print("Created table successfully........")
 # conn.commit()
 
-for i in range(1000):
-    temp, hum, dust, co2 = random_variables()
-    c.execute('''INSERT INTO MEASUREMENTS(TEMPERATURE, HUMIDITY, DUST, CO2) 
-            VALUES (?, ?, ?, ?)''',
-         (temp, hum, dust, co2))
-    print("Inserted data successfully........")
+sql2 = '''CREATE TABLE DEVICES(
+    DEVICE_ID INTEGER PRIMARY KEY
+'''
+
+add_keys = '''ALTER TABLE MEASUREMENTS ADD FOREIGN KEY (DEVICE_ID) INTEGER REFERENCES DEVICES(DEVICE_ID)'''
+
+c.execute(sql2)
+
+c.execute()
+
+def device1data():
+    dev1_id = 1
+    for i in range(1000):
+        temp, hum, dust, co2 = random_variables()
+        c.execute('''INSERT INTO MEASUREMENTS(DEVICE_ID, TEMPERATURE, HUMIDITY, DUST, CO2) 
+                VALUES (?, ?, ?, ?, ?)''',
+            (dev1_id, temp, hum, dust, co2))
+        print("Inserted data successfully........")
+    conn.commit()
+
+def device2data():
+    dev2_id = 2
+    for i in range(1000):
+        temp, hum, dust, co2 = random_variables()
+        c.execute('''INSERT INTO MEASUREMENTS(DEVICE_ID, TEMPERATURE, HUMIDITY, DUST, CO2) 
+                VALUES (?, ?, ?, ?, ?)''',
+            (dev2_id, temp, hum, dust, co2))
+        print("Inserted data successfully........")
+    conn.commit()
+
+device1data()
+device2data()
 
 c.execute("SELECT * FROM MEASUREMENTS")
 data = c.fetchall()
